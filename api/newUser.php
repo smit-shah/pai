af4bd209fd62bb6239581fd012b74e636ab5bbc7
Smit Shah <smit.v.shah@gmail.com>
2016-10-22 19:37:20 +0530

    Changes in registration

--- api/newUser.php
+++ api/newUser.php
@@ -1,15 +1,18 @@
 <?php
     require_once 'config.php';
+    global $wpdb;
 
-    $first_name  =   sanitize_text_field($_REQUEST['first_name']);
-    $last_name =   sanitize_text_field($_REQUEST['last_name']);
-    $email =   sanitize_text_field($_REQUEST['email']);
-    $device_id =   sanitize_text_field($_REQUEST['device_id']);
-    $password =   sanitize_text_field($_REQUEST['password']);
-    $platform =   sanitize_text_field($_REQUEST['platform']);
-    $gender =   sanitize_text_field($_REQUEST['gender']);
-    $age =   sanitize_text_field($_REQUEST['age']);
+    $first_name  = sanitize_text_field($_REQUEST['first_name']);
+    $last_name = sanitize_text_field($_REQUEST['last_name']);
+    $email = sanitize_text_field($_REQUEST['email']);
+    $device_id = sanitize_text_field($_REQUEST['device_id']);
+    $password = sanitize_text_field($_REQUEST['password']);
+    $platform = sanitize_text_field($_REQUEST['platform']);
+    $gender = sanitize_text_field($_REQUEST['gender']);
+    $age = sanitize_text_field($_REQUEST['age']);
+    $referal = sanitize_text_field($_REQUEST['referal']);
     $invalid_usernames = array('admin');
+    $code = substr($first_name, 0, 3) . substr(time(), -4);
     
     if (!validate_username($first_name) || in_array($first_name, $invalid_usernames)) {
     	echo json_encode(array('status' => 'fail', 'message' => 'First name is invalid.'));
@@ -63,25 +66,39 @@
                 $imagepath          =   $upload_dir['path'] . '/';
                 $httpimagepath      =   $upload_dir['url'] . '/';
                 move_uploaded_file($tmp_name, $imagepath . $image_name);
-                update_user_meta($user_id, 'profile_img', $httpimagepath . $image_name);
+                add_user_meta($user_id, 'profile_img', $httpimagepath . $image_name);
                 $result_array['profile_image'] = $httpimagepath . $image_name;
             }
             else {
                 $result_array['profile_image'] = "";
             }
-            update_user_meta($user_id, 'device_id', $device_id);
-            update_user_meta($user_id, 'platform', $platform);
-            update_user_meta($user_id, 'first_name', $first_name);
-            update_user_meta($user_id, 'last_name', $last_name);
-            update_user_meta($user_id, 'user_password', $password);
-            update_user_meta($user_id, 'gender', $gender);
-            update_user_meta($user_id, 'age', $age);
-            update_user_meta($user_id, 'status', 1);
-            update_user_meta($userdata->ID, 'lat', $_REQUEST['lat']);
-            update_user_meta($userdata->ID, 'lon', $_REQUEST['lon']);
+
+            add_user_meta($user_id, 'device_id', $device_id);
+            add_user_meta($user_id, 'platform', $platform);
+            add_user_meta($user_id, 'first_name', $first_name);
+            add_user_meta($user_id, 'last_name', $last_name);
+            add_user_meta($user_id, 'user_password', $password);
+            add_user_meta($user_id, 'gender', $gender);
+            add_user_meta($user_id, 'age', $age);
+            add_user_meta($user_id, 'status', 1);
+            add_user_meta($user_id, 'lat', $_REQUEST['lat']);
+            add_user_meta($user_id, 'lon', $_REQUEST['lon']);
+            add_user_meta($user_id, 'code', $code);
+
+            if ($referal != '') {
+                $existing_code = $wpdb->get_row('SELECT * FROM `' . _get_meta_table('user') . '` WHERE `meta_key` LIKE "code" AND `meta_value` LIKE "'.$referal.'" ', ARRAY_A);
+                if (count($existing_code) > 0) {
+                    $result_array['invalid_code'] = 0;
+                    add_user_meta($user_id, 'referal_user_id', $existing_code['user_id']);
+                    add_user_meta($user_id, 'used_referal', 1);
+                }
+                else {
+                    $result_array['invalid_code'] = 1;
+                }
+            }
             
-            $message                    =   'New user created successfully';
-            $result_return                  =   array('data' => $result_array,'status'=>'success','message'=>$message);
+            $message = 'New user created successfully';
+            $result_return =  array('data' => $result_array,'status'=>'success','message'=>$message);
     		echo json_encode($result_return);
     		die();
         }
